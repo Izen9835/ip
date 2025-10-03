ced62eeeaaddb47503a333945c311cb2935cb0e1
Izen9835 <e1406600@u.nus.edu>
2025-10-03 22:55:01 +0800

    Update runtest to include new commands

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,14 +13,14 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/Bart/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the EXPECTED.TXT
-java -classpath ../bin Duke < input.txt > EXPECTED.TXT
+java -classpath ../bin Bart.Bart < input.txt > EXPECTED.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
